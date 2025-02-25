f41635c7aa11beb2ad8173509ab49f7e6464d26b
ghandic <andy.challis@capgemini.com>
2025-02-25 21:56:50 +1100

    Fix base url on deployment

--- src/main.tsx
+++ src/main.tsx
@@ -5,16 +5,16 @@ import {
 } from "@/components/ui/tooltip"
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter } from "react-router";
+import { HashRouter } from "react-router";
 
 import App from './App.tsx';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <BrowserRouter basename={import.meta.env.BASE_URL}>
+    <HashRouter basename={import.meta.env.BASE_URL}>
       <TooltipProvider>
         <App />
       </TooltipProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>,
 );
--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/game-time/",
+  base: process.env.NODE_ENV === 'production' ? "/game-time/" : "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
