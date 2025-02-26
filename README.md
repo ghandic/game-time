04c4855290a1efd6ce21f6f18d54365b37293ebc
ghandic <andy.challis@capgemini.com>
2025-02-26 20:57:21 +1100

    add code link and issue link

--- README.md
+++ README.md
@@ -1,10 +1,8 @@
 # Game Time
 
-Check out the games at [ghandic.github.io/game-time](https://ghandic.github.io/game-time/)
+Check out the games at [game-time.andrewchallis.com](https://game-time.andrewchallis.com/)
 
 ## Ideas
 
 - Allow for multiple rulesets
 - Make animations slightly nicer
-- Add link to issue tracker on github
-- Cheap domain
--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import { AppSidebar } from "@/components/app-sidebar"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { Bug, Github, HelpCircle } from "lucide-react";
 import { Route, Routes } from "react-router";
 
+import { Button } from "./components/ui/button";
 import Scoundrel from './games/scoundrel';
 
 function Home() {
@@ -20,7 +30,26 @@ function App() {
     <div className="w-full"><SidebarProvider>
       <AppSidebar />
       <main className="w-full">
-        <SidebarTrigger />
+        <div className="flex flex-row justify-between">
+          <SidebarTrigger />
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild><Button
+              data-sidebar="trigger"
+              data-slot="sidebar-trigger"
+              variant="ghost"
+              size="icon"
+              className="h-7 w-7"
+            >
+              <HelpCircle />
+              <span className="sr-only">Help</span>
+            </Button></DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuItem><a href="https://github.com/ghandic/game-time" target="_blank"><div className="flex flex-row gap-2 items-center"><Github /><div>View the code</div></div></a></DropdownMenuItem>
+              <DropdownMenuItem><a href="https://github.com/ghandic/game-time/issues/new" target="_blank"><div className="flex flex-row gap-2 items-center"><Bug /><div>Report an issue</div></div></a></DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+
+        </div>
         <div className="min-h-screen p-4 bg-gray-100">
           <Routes >
             <Route index element={<Home />} />
