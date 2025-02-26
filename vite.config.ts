99cd0aace3d5db6f8288e2560bdf742c1c2767a3
ghandic <andy.challis@capgemini.com>
2025-02-26 20:43:09 +1100

    add custom url

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import { defineConfig } from 'vite';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: process.env.NODE_ENV === 'production' ? "/game-time/" : "/game-time",
+  // base: process.env.NODE_ENV === 'production' ? "/game-time/" : "/game-time",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
